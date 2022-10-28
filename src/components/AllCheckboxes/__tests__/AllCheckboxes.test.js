ee841cda26a910418a2b207f07f45c53000538fd
Dawid Haratyk <dharatyk@onet.pl>
2022-10-28 19:19:57 +0200

    tests: add test to AllCheckboxes component

--- src/components/AllCheckboxes/__tests__/AllCheckboxes.test.js
+++ src/components/AllCheckboxes/__tests__/AllCheckboxes.test.js
@@ -1,33 +1,51 @@
-import { screen, within } from "@testing-library/react";
-import { renderComponent } from "utils/renderComponent";
-import AllCheckboxes from "../AllCheckboxes";
+import { screen, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderComponent } from 'utils/renderComponent'
+import AllCheckboxes from '../AllCheckboxes'
 
-test("cos", () => {
-  // naucz się mockować funkcje
+describe('AllCheckboxes', () => {
+  test('cos', () => {
+    const mockSetAllCheckboxes = jest.fn()
+    const mockSetFilters = jest.fn()
 
-  renderComponent(
-    <AllCheckboxes
-      allCheckboxes={[
-        {
-          priceOrder: "ascending",
-          isChecked: true,
-          id: 0,
-        },
-        {
-          priceOrder: "descending",
-          isChecked: false,
-          id: 1,
-        },
-      ]}
-      setAllCheckboxes={() => {}}
-      setFilters={() => {}}
-    />
-  );
+    renderComponent(
+      <AllCheckboxes
+        allCheckboxes={[
+          {
+            priceOrder: 'ascending',
+            isChecked: true,
+            id: 0,
+          },
+          {
+            priceOrder: 'descending',
+            isChecked: false,
+            id: 1,
+          },
+        ]}
+        setAllCheckboxes={mockSetAllCheckboxes}
+        setFilters={mockSetFilters}
+      />
+    )
 
-  const allCheckboxes = screen.getAllByRole("checkbox");
-  expect(allCheckboxes).toHaveLength(2);
+    const allCheckboxes = screen.getAllByRole('checkbox')
+    expect(allCheckboxes).toHaveLength(2)
 
-  const withinFirstCheckboxView = screen.getByText(/ascending/i);
-  const firstCheckbox = within(withinFirstCheckboxView).getByRole("checkbox");
-  expect(firstCheckbox).not.toBeChecked();
-});
+    const withinFirstCheckboxView = screen.getByText(/ascending/i)
+    const firstCheckbox = within(withinFirstCheckboxView).getByRole('checkbox')
+    expect(firstCheckbox).toBeChecked()
+
+    const withinSecondCheckboxView = screen.getByText(/descending/i)
+    const secondCheckbox = within(withinSecondCheckboxView).getByRole(
+      'checkbox'
+    )
+    expect(secondCheckbox).not.toBeChecked()
+
+    userEvent.click(firstCheckbox)
+    expect(mockSetAllCheckboxes).toHaveBeenCalledTimes(1)
+    expect(mockSetFilters).toHaveBeenCalledTimes(0)
+
+    userEvent.click(secondCheckbox)
+    expect(mockSetAllCheckboxes).toHaveBeenCalledTimes(2)
+    expect(mockSetFilters).toHaveBeenCalledTimes(1)
+  })
+})
